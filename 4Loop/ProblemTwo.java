bbb7fa032244b869a7d34b5ceaa64e4439939f25
Faraz Alam <farazalam2017@gmail.com>
2024-01-31 20:00:31 +0530

    loop completed

--- 4Loop/ProblemTwo.java
+++ 4Loop/ProblemTwo.java
@@ -3,14 +3,13 @@ import java.util.*;
 public class ProblemTwo {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    do{
+    do {
       System.out.println("Enter the number:- ");
-      int num1= sc.nextInt();
-      if(num1 %10 ==0){
+      int num1 = sc.nextInt();
+      if (num1 % 10 == 0) {
         continue;
       }
       System.out.println(num1);
     } while (true);
   }
-
 }
