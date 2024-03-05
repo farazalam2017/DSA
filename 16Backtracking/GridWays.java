007cfe5e1381923e7a00e4791d6858b6f2025d96
Faraz Alam <farazalam2017@gmail.com>
2024-03-05 20:15:32 +0530

    Linked List

--- 16Backtracking/GridWays.java
+++ 16Backtracking/GridWays.java
@@ -1,4 +1,4 @@
-/* Find he number of ways to reach from (0,0) to (n-1,m-1) in a n*m Grid */
+/* Find he number of ways to reach from (0,0) to (n-1,m-1) in a n*m Grid, if allowed moves are only right and down */
 /* Below approach gives complexity of O(2^(n+m)), exponential */
 /* public class GridWays {
   public static int gridWays(int i, int j, int n, int m) {
@@ -19,7 +19,7 @@
   }
 } */
 /* Below approach gives complexity of O(n+m), linear time */
-/* In grid of N*M , number of ways will be (N+M)!/N!*M! */
+/* In grid of N*M , number of ways will be (N-1+M-1)!/(N-1)!*(M-1)! */
 public class GridWays {
   public static long factorial(int N) {
     int result = 1;
@@ -29,9 +29,16 @@ public class GridWays {
     }
     return result;
   }
+
+  public static long countWays(int N, int M) {
+    long total = factorial(N-1+M-1);
+    long total1 = factorial(N-1);
+    long total2 = factorial(M-1);
+    return total / (total1 * total2);
+  }
 public static void main(String[] args) {
   int n = 3;
   int m = 3;
-  System.out.print("Ways:- "+)
+  System.out.print("Ways:- " + countWays(n, m));
 }
 }
